f0e3350f23cb3a7470da955c8c85cbb6d7de3151
gsingh-0-0-1 <gurmehar@gmail.com>
2024-05-07 17:26:42 -0700

    basic server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const fs = require('fs')
+const socketIO = require('socket.io');
+
+
+const PORT = 3000;
+const HOST = "localhost"
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(PORT, HOST, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
